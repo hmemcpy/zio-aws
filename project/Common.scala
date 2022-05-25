f50aa4945068141cf9e10247313efdb6dbc90a7c
Daniel Vigovszky <daniel.vigovszky@gmail.com>
2022-05-25 03:45:34 +0000

    Update http4s-dsl to 0.23.12

--- project/Common.scala
+++ project/Common.scala
@@ -19,7 +19,7 @@ object Common extends AutoPlugin {
 
     val awsVersion = "2.17.104"
     val awsSubVersion = awsVersion.drop(awsVersion.indexOf('.') + 1)
-    val http4sVersion = "0.23.11"
+    val http4sVersion = "0.23.12"
     val fs2Version = "3.2.5"
 
     val majorVersion = "3"
